cbc89ed4e2273665676b6f20d16fb61d54311c73
sledopitka <sledopitka@gmail.com>
2022-05-17 10:14:17 +0300

    Initial commit

---
+++ src/main/java/ru/novikova/fermer_marketplace/FermerMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package ru.novikova.fermer_marketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FermerMarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FermerMarketplaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/novikova/fermer_marketplace/FermerMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.novikova.fermer_marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FermerMarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
